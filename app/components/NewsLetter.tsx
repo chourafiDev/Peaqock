cbffb59a1e56a15922598f4e892e9120f5df3346
chourafiDev <abdelmonaimechourafi@gmail.com>
2022-10-14 14:51:52 +0100

    Fix Responsive Footer

--- app/components/NewsLetter.tsx
+++ app/components/NewsLetter.tsx
@@ -3,18 +3,18 @@ import Button from "./Utils/Button";
 
 const NewsLetter = () => {
   return (
-    <div className="bg-light-gray px-20 py-16 flex justify-between items-center">
+    <div className="bg-light-gray px-20 py-16 flex lg:flex-row flex-col justify-between items-center gap-10">
       <div>
         <h2 className="relative uppercase text-3xl font-bold text-base-gray before:absolute before:-bottom-3 before:left-0 before:bg-primary before:h-1 before:w-20 before:rounded-full">
           NewsLetter
         </h2>
       </div>
       <div>
-        <div className="flex items-center bg-forth-gray rounded-full">
+        <div className="flex md:flex-row flex-col items-center md:bg-forth-gray rounded-full mx-8">
           <input
             type="text"
             placeholder="Your email adress"
-            className="bg-transparent outline-none px-6 w-[28rem] text-md text-second-gray"
+            className="bg-transparent outline-none px-6 md:w-[28rem] w-[23rem] text-md text-second-gray border border-second-gray md:py-0 py-4 rounded-full md:mb-0 mb-3"
           />
           <Button cssClasses="flex items-center gap-2 btn btn-green font-normal">
             <span>Subscribe</span>
--- app/core/Layout/Footer/index.tsx
+++ app/core/Layout/Footer/index.tsx
@@ -9,46 +9,45 @@ import Button from "../../../components/Utils/Button";
 
 const Footer = () => {
   return (
-    <div className="bg-forth-gray px-20">
+    <div className="bg-forth-gray md:px-20 px-5">
       <div className="py-12">
-        <div className="flex justify-end">
-          <div className="flex items-center gap-3">
-            <Button cssClasses="btn btn-yellow flex items-center gap-2 px-4">
-              <BiMenuAltLeft className="font-bold" />
-              <span className="font-normal">Support</span>
-            </Button>
-            <Button cssClasses="btn btn-yellow rounded-full p-2">
-              <AiOutlineArrowUp className="font-bold" size={25} />
-            </Button>
-          </div>
+        <div className="flex justify-end items-center gap-3 mb-6">
+          <Button cssClasses="btn btn-yellow flex items-center gap-2 px-4">
+            <BiMenuAltLeft className="font-bold" />
+            <span className="font-normal">Support</span>
+          </Button>
+          <Button cssClasses="btn btn-yellow rounded-full p-2">
+            <AiOutlineArrowUp className="font-bold" size={25} />
+          </Button>
         </div>
-        <div className="grid grid-cols-4">
-          <div className="col-span-2 pr-36">
+
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-8 lg:gap-0">
+          <div className="lg:col-span-2 lg:pr-36">
             <h3 className="text-primary font-bold text-xl pb-6">Contact Us</h3>
-            <p className="base-gray font-light text-[15px] pr-28">
+            <p className="base-gray font-light text-[15px] lg:pr-28">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
               itaque magni nisi doloribus voluptatem nostrum ad ex quidem?
             </p>
             <ul className="flex flex-col gap-4 mt-10 text-[15px]">
               <li className="flex gap-3">
-                <FiMapPin size={20} className="text-primary mt-0" />
+                <FiMapPin className="text-primary mt-0 text-4xl" />
                 <p className="base-gray font-light text-sm">
                   Imm le Zenith II, Lotissement Attaoufik, Route de Nouacer,
                   Sidi Maarouf, Casablanca, 20640, Maroc
                 </p>
               </li>
               <li className="flex items-center gap-3">
-                <BiEnvelope className="text-primary" />
+                <BiEnvelope className="text-primary text-xl" />
                 <p className="base-gray font-light text-sm">nfo@peaqock.com</p>
               </li>
               <li className="flex items-center gap-3">
-                <BsTelephone className="text-primary" />
+                <BsTelephone className="text-primary text-xl" />
                 <p className="base-gray font-light text-sm">
                   +20-2-24564100/1/2/3
                 </p>
               </li>
               <li className="flex items-center gap-3">
-                <BsTelephone className="text-primary" />
+                <BsTelephone className="text-primary text-xl" />
                 <p className="base-gray font-light text-sm">+20-2-24515201/2</p>
               </li>
             </ul>
